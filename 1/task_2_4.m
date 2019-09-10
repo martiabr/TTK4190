4f2763c2d942b09ee448dd25050f26ac6901aea5
Ivan Kingman <ivanhk@stud.ntnu.no>
2019-09-10 13:04:30 +0200

    Added matlab and simulink for task 2

---
+++ 1/task_2_4.m
@@ -0,0 +1,19 @@
+%% 2. order Nomoto constant bearing
+
+clear all;
+
+%% Parameters
+U = 5;
+x_0 = 0;
+y_0 = 100;
+psi_0 = 0;
+r_0 = 0;
+T = 20;
+K = 0.1;
+b = 0.001;
+
+%% :(
+
+k_p = 1*10^-8;
+k_i = 0.5*10^-7; 
+k_d = 2*10^-0;
